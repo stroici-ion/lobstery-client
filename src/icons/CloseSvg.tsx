3f7a68b8c6c47ade4cb09a29ed3458d0aac46c10
stroici-ion <stroici.ion.22@gmail.com>
2024-08-21 12:30:27 +0300

    Update Close Button

---
+++ src/icons/CloseSvg.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CloseSvg: React.FC = () => {
+  return (
+    <svg width="43" height="43" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M4 4L39.3553 39.3553" stroke="black" strokeWidth="7" strokeLinecap="round" />
+      <path d="M39.3553 4L4.00001 39.3553" stroke="black" strokeWidth="7" strokeLinecap="round" />
+    </svg>
+  );
+};
+
+export default CloseSvg;
