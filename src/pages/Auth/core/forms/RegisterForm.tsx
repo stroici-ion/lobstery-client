9a09c1dc7880c14d722c4db5949be9d647d231fa
stroici-ion <stroici.ion.22@gmail.com>
2024-09-09 00:17:04 +0300

    Dep 1

--- src/pages/Auth/core/forms/RegisterForm.tsx
+++ src/pages/Auth/core/forms/RegisterForm.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import classNames from 'classnames';
-import { connect } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 
 import styles from './styles.module.scss';
 import { EnumFromTypes } from '../..';
--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
+import classNames from 'classnames';
 
 import styles from './styles.module.scss';
-import classNames from 'classnames';
 import LoginForm from './Core/Forms/LoginForm';
 import RegisterForm from './Core/Forms/RegisterForm';
 
